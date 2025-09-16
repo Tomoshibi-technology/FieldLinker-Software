f2b7417614a1f24f4ee139558d2ed9210063977e
Jun-robot <saijun721.1@gmail.com>
2025-09-16 22:07:39 +0900

    add ui-cubeide

---
+++ ui-test-cubeide/User/Inc/user_app.h
@@ -0,0 +1,15 @@
+#ifndef _USER_APP_H_
+#define _USER_APP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void UserApp_Init(void);
+void UserApp_Run(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __USER_APP_H */
